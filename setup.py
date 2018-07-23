e63815d23e936128e217454e3b64a16885c0139f
Trent Bullard <trent.bullard@dhsgroup.com>
2018-07-23 09:15:02 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-polls',
+    name='django-polls-trentbullard',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    license='CC0 License',
+    license='MIT License',
     description='A simple Django app to conduct Web-based polls.',
     long_description=README,
     url='https://github.com/trentbullard',
